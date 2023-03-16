3df07499cf8c06bb21a7b7855b3e5107ac2211cc
Behnam Mohammadi <behnam.modi@outlook.com>
2023-03-16 17:31:05 +0100

    fix react type

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-import { createElement, ReactElement } from "react";
-import type { ReactNode } from "react";
+import { createElement } from "react";
+import type { FunctionComponent } from "react";
+import { elements } from "./elements";
 
-type TailwindyComponent = ReactNode & {
+type TailwindyComponent = FunctionComponent & {
   __TAILWINDY__: {
     classes: string;
-    element: ReactNode;
+    element?: FunctionComponent;
   };
 };
 
 export type TailwindyElements = {
   [K in typeof elements[number]]: ([
     classes,
-  ]: TemplateStringsArray) => ReactElement<JSX.IntrinsicElements[K]>;
+  ]: TemplateStringsArray) => FunctionComponent<JSX.IntrinsicElements[K]>;
 };
 
 export type TailwindyConstructor = (
-  element: ReactElement | TailwindyComponent
+  element: FunctionComponent | TailwindyComponent
 ) => ([classes]: TemplateStringsArray) => TailwindyComponent;
 
 export type Tailwindy = TailwindyConstructor & TailwindyElements;
 
 const createTailwindyComponents = (elements) => {
   const createTailwindy = (element) => (classes: string) => {
-    function Component({ className, ...props }: { className: string }) {
+    const Component: TailwindyComponent = ({
+      className,
+      ...props
+    }: {
+      className: string;
+    }) => {
       const cn = `${classes} ${className}`;
 
       return createElement(element, {
         className: cn,
         ...props,
       });
-    }
+    };
 
     Component.__TAILWINDY__ = {
       classes,
@@ -60,144 +66,6 @@ const createTailwindyComponents = (elements) => {
 
   return tailwindy as Tailwindy;
 };
-
-const elements = [
-  "a",
-  "abbr",
-  "address",
-  "area",
-  "article",
-  "aside",
-  "audio",
-  "b",
-  "base",
-  "bdi",
-  "bdo",
-  "big",
-  "blockquote",
-  "body",
-  "br",
-  "button",
-  "canvas",
-  "caption",
-  "cite",
-  "code",
-  "col",
-  "colgroup",
-  "data",
-  "datalist",
-  "dd",
-  "del",
-  "details",
-  "dfn",
-  "dialog",
-  "div",
-  "dl",
-  "dt",
-  "em",
-  "embed",
-  "fieldset",
-  "figcaption",
-  "figure",
-  "footer",
-  "form",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "head",
-  "header",
-  "hgroup",
-  "hr",
-  "html",
-  "i",
-  "iframe",
-  "img",
-  "input",
-  "ins",
-  "kbd",
-  "keygen",
-  "label",
-  "legend",
-  "li",
-  "link",
-  "main",
-  "map",
-  "mark",
-  "menu",
-  "menuitem",
-  "meta",
-  "meter",
-  "nav",
-  "noscript",
-  "object",
-  "ol",
-  "optgroup",
-  "option",
-  "output",
-  "p",
-  "param",
-  "picture",
-  "pre",
-  "progress",
-  "q",
-  "rp",
-  "rt",
-  "ruby",
-  "s",
-  "samp",
-  "script",
-  "section",
-  "select",
-  "small",
-  "source",
-  "span",
-  "strong",
-  "style",
-  "sub",
-  "summary",
-  "sup",
-  "table",
-  "tbody",
-  "td",
-  "textarea",
-  "tfoot",
-  "th",
-  "thead",
-  "time",
-  "title",
-  "tr",
-  "track",
-  "u",
-  "ul",
-  "use",
-  "var",
-  "video",
-  "wbr", // SVG
-  "circle",
-  "clipPath",
-  "defs",
-  "ellipse",
-  "foreignObject",
-  "g",
-  "image",
-  "line",
-  "linearGradient",
-  "marker",
-  "mask",
-  "path",
-  "pattern",
-  "polygon",
-  "polyline",
-  "radialGradient",
-  "rect",
-  "stop",
-  "svg",
-  "text",
-  "tspan",
-] as const;
 
 const tailwindy = createTailwindyComponents(elements);
 
